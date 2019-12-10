7f19d6148d7b2336c5f6a7550f4119edc9383114
viki korso <viki.korso@gmail.com>
2019-12-10 20:59:17 +0200

    slider

---
+++ slider.js
@@ -0,0 +1,11 @@
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 4,
+    // spaceBetween: 10,
+    slidesOffsetBefore: 135,
+    slidesOffsetAfter: 135,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
